4da54bbb904e7d22d5a2d0f33ec47bcf1252bef3
Abdellatif <abdelt.2012@gmail.com>
2024-06-11 22:40:24 +0100

    Done.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ yield a random number between 0 and 10. Use the random module.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None,  None]:
     """
     An asynchronous generator that yields a random float between 0 and 10 every
     second.
